31b568dfedcc53d0e6100abd02caf2f62ba6d83f
Dawson Walker <dw@autocrm.io>
2017-01-09 20:10:28 +0100

    updated pages

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -10,7 +10,7 @@ const animation = merge(flash, shake);
 const closeanimation = merge(rotateOut, rotateIn);
 const CLOUDINARY_UPLOAD_PRESET = 'upload';
 const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/ddaohvlb0/upload';
-const postURL = 'https://beverlywalker.herokuapp.com'; //'http://localhost:3000'; for local testing.
+const postURL = 'http://localhost:3000'; //'https://beverlywalker.herokuapp.com'; //'http://localhost:3000'; for local testing.
 
 
 export default class AdminPage extends Component {
