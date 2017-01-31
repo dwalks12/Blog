24d98d96a15c15f9a8bcccd98238d76f181db07d
Dawson Walker <dw@autocrm.io>
2017-01-31 08:22:47 +0100

    updated webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -35,6 +35,11 @@ var config = {
         compress: {
           warnings: false
         }
+      }), new Webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production'),
+          'API_URL': JSON.stringify('https://beverlywalker.herokuapp.com')
+        }
       })]
 };
 
